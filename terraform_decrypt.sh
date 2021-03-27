e98850da9206fb3ecb25d9c33466c4835fe29dac
jworl <jworl@users.noreply.github.com>
2021-03-27 14:10:43 -0400

    Update terraform_decrypt.sh
    check for empty resources (happens post destroy).

--- terraform_decrypt.sh
+++ terraform_decrypt.sh
@@ -1,13 +1,17 @@
 #!/usr/bin/env bash
 
+# author: Joshua Worley
+# contact: joshua.worley@warnermedia.com
+
 help() {
     echo ${0##*/} usage
-    echo "-k, --key  : private key for decrypting passphrase"
+    echo "-k, --key     : path private key for decrypting passphrase"
+    echo "-t, --tfstate : path to terraform.tfstate file"
     exit 2
 }
 
 DECRYPT() {
-    DATA=$(jq '[.resources[] | select(.type=="aws_instance") | .instances[].attributes | select(.password_data!="")]' terraform.tfstate)
+    DATA=$(jq '[.resources[] | select(.type=="aws_instance") | .instances[].attributes | select(.password_data!="")]' ${2})
 
     for a in $(seq 0 `echo $DATA | jq length-1`); do
         ID=$(echo $DATA | jq -r ".[$a] | .id")
@@ -21,6 +25,7 @@ DECRYPT() {
     done
 }
 
+TFSTATE="terraform.tfstate"
 
 while (( "$#" )); do
     case $1 in
@@ -32,6 +37,14 @@ while (( "$#" )); do
             fi
             shift
         ;;
+        -t|--tfstate)
+            if [ -z ${2} ]; then
+                help
+            else
+                TFSTATE=$2
+            fi
+            shift
+        ;;
         *)
             help
         ;;
@@ -39,4 +52,14 @@ while (( "$#" )); do
     shift
 done
 
-DECRYPT $KEY
+if [ -f ${TFSTATE} ]; then
+    if [[ $(jq '.resources | length' ${TFSTATE}) -eq 0 ]]; then
+        echo "[!] ${TFSTATE} has 0 resources"
+        exit 2
+    fi
+    DECRYPT $KEY $TFSTATE
+else
+    echo "[!] did not find terraform.tfstate file."
+    echo "[!] is this script in the same directory?"
+    echo "[!] did you provide a valid path?"
+fi
